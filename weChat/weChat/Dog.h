a79b01031bfd386fdb9b334335f0d0f252de070a
XMG <XMG@163.com>
2016-11-05 15:05:50 +0800

    创建了 Dog类

---
+++ weChat/weChat/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weChat
+//
+//  Created by lanou3g on 16/11/5.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weChat/weChat/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weChat
+//
+//  Created by lanou3g on 16/11/5.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
