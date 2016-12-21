51b1df082082b2b3e28eb5d8aa16c2ac99f34e98
lanou3g <lanou3g@lanou3g.local>
2016-12-21 20:51:01 +0800

    sdfji

---
+++ weChat/weChat/erjgtidgsViewController.h
@@ -0,0 +1,13 @@
+//
+//  erjgtidgsViewController.h
+//  weChat
+//
+//  Created by lanou3g on 16/12/21.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface erjgtidgsViewController : UIViewController
+
+@end
---
+++ weChat/weChat/erjgtidgsViewController.m
@@ -0,0 +1,37 @@
+//
+//  erjgtidgsViewController.m
+//  weChat
+//
+//  Created by lanou3g on 16/12/21.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import "erjgtidgsViewController.h"
+
+@interface erjgtidgsViewController ()
+
+@end
+
+@implementation erjgtidgsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
